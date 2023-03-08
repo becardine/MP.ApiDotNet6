985ed57df1fb585014f3906991fa6cc31dbb154a
becardine <bealvescardine@gmail.com>
2023-03-07 22:26:30 -0300

    feat: dtos and validation

---
+++ MP.ApiDotNet6.Application/DTOs/Validations/PersonDTOValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP.ApiDotNet6.Application.DTOs.Validations
+{
+    public class PersonDTOValidator : AbstractValidator<PersonDTO>
+    {
+        public PersonDTOValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty().NotNull().WithMessage("Nome deve ser informado!");
+            RuleFor(p => p.Document).NotEmpty().NotNull().WithMessage("Documento deve ser informado!");
+            RuleFor(p => p.Phone).NotEmpty().NotNull().WithMessage("Telefone deve ser informado!");
+        }
+    }
+}
